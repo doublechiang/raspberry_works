aa77b61a50265d70399050166e3251ed11bdddec
Jiang Junyu <double.chiang@gmail.com>
2015-08-22 21:41:01 +0800

    GPIO detection changed from polling to edge triggered method.

--- mp3/daemon.py
+++ mp3/daemon.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
+# Reference:
+# callback button event refer to http://makezine.com/projects/tutorial-raspberry-pi-gpio-pins-and-python/
+# Python dbus control play back.
+
 # Todo
-# 1. event callback
 # 2. initialize music list, press power button to start/stop replay music
 # 3. volume up/down control
 
@@ -11,7 +14,7 @@ import time
 import os
 import logging
 
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.basicConfig(level=logging.INFO)
 
 # initialize GPIO
 GPIO.setmode(GPIO.BCM)
@@ -29,23 +32,51 @@ GPIO.setup(volumeup, GPIO.IN, GPIO.PUD_UP)
 GPIO.setup(volumedown, GPIO.IN, GPIO.PUD_UP)
 
 powercnt=0
+powerDownTime=0.0
 
 # light it up
 GPIO.output(powerled, 1)
 
-while True:
-    if GPIO.input(powerbutton) ==False:
-        # power butten pressed
-        logging.debug("power button down!")
-        powercnt +=1
-    else:
-        logging.debug ("power button up!")
-        powercnt = 0
+# callback function for volumn down
+def volDown(channel):
+    pass
+    print "VolDown"
 
-    if (powercnt >3):
-        logging.debug( "Power button pressed for over 3 seconds, shutdown now!")
-        os.system("init 0")
+def volUp(channel):
+    pass
+    print "VolUp"
+
+
+def start_stop():
+    print "start/stop action"
+    pass
+
+
+
+def powerButtonEvent(channel):
+    global powerDownTime
+    if GPIO.input(powerbutton) == False:
+        powerDownTime = time.time()
+    else:
+        pressTime = time.time() - powerDownTime
+        print "Power button down time %f" % (time.time() - powerDownTime)
+        if pressTime > 3.0 :
+            print "PowerDown"
+            os.system("init 0")
+        else:
+            start_stop
+        
+    pass
+
+
+# detect RISING  
+
+GPIO.add_event_detect(powerbutton, GPIO.BOTH, callback=powerButtonEvent)
+GPIO.add_event_detect(volumeup, GPIO.RISING, callback=volUp)
+GPIO.add_event_detect(volumedown, GPIO.RISING, callback=volDown)
+while True:
     time.sleep(1)
+
     
 
 #clean it up
